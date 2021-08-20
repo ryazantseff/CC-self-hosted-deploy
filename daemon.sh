17b778147fdaf3b0e46022f915d6e03ca26b7119
Max Ryazantseff <maxim.ryazancev@gmail.com>
2021-08-20 18:07:20 +0300

    init

---
+++ daemon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source .env
+export $(cut -d= -f1 .env)
+docker-compose -f docker-compose.deploy.yml config
+docker-compose -f docker-compose.deploy.yml down
+docker-compose -f docker-compose.deploy.yml pull
+docker-compose -f docker-compose.deploy.yml up -d
